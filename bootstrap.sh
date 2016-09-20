671b948ca56122d2bfc255862227dcfc2e0e09ae
Alpha Chen <achen@pivotal.io>
2016-09-20 10:27:42 -0700

    Add a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ansible
+mkdir ~/workspace
+git clone https://github.com/seattle-beach/alfalfa.git ~/workspace/alfalfa
