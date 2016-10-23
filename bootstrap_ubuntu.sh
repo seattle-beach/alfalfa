eed93f4b51c7f0935121483d8fcc9bcdcfadd889
Liz Dahlstrom <tanglisha@gmail.com>
2016-10-22 17:20:06 -0700

    added bootstrap for ubuntu

---
+++ bootstrap_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y git ansible sshpass
+hash foo 2>/dev/null         || brew install ansible
+[[ -d ~/workspace ]]         || mkdir ~/workspace
+[[ -d ~/workspace/alfalfa ]] || git clone https://github.com/seattle-beach/alfalfa.git ~/workspace/alfalfa
+ansible-playbook ~/workspace/alfalfa/ansible/main.yml -i "localhost" -c local --ask-pass --ask-become-pass
