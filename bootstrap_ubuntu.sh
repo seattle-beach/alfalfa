071354ce315a4d5e95d024b23cf5785b18a68a6e
Liz Dahlstrom <tanglisha@gmail.com>
2016-10-22 17:23:54 -0700

    moved python-apt to setup bootstrap

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y git ansible sshpass
+sudo apt-get install -y git ansible sshpass python-apt
 hash foo 2>/dev/null         || brew install ansible
 [[ -d ~/workspace ]]         || mkdir ~/workspace
 [[ -d ~/workspace/alfalfa ]] || git clone https://github.com/seattle-beach/alfalfa.git ~/workspace/alfalfa
