e5ee38cf59501fb5f3d545adc7425dd1f0f209c6
Andres Medina <amedina@pivotal.io>
2017-06-19 16:24:51 -0700

    moved rust stuff onto a separate playbook

--- rust.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH="$HOME/.cargo/bin:$PATH"
-rustup default nightly
-rustup component add rust-src
-cargo install cargo-update
-cargo install racer
-cargo install rustfmt-nightly
-cargo install clippy
