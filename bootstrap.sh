e10a25f48f2b0a1be7cb352b7c4226f80f592dda
Liz Dahlstrom <edahlstrom@pivotal.io>
2017-10-03 16:29:28 -0700

    Added some sane bash aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+echo "[defaults]
+host_key_checking = False" > ~/ansible.cfg
 [[ -d /usr/local/Homebrew ]] || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-hash foo 2>/dev/null         || brew install ansible
+
+if hash ansible; then
+  echo "Ansible already installed. Skipping."
+  else
+    brew install ansible
+fi
+
 [[ -d ~/workspace ]]         || mkdir ~/workspace
 [[ -d ~/workspace/alfalfa ]] || git clone https://github.com/seattle-beach/alfalfa.git ~/workspace/alfalfa
