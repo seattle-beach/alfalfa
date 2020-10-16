caa3449f21ea50460b4cdcbbe4491fcc88f4ae75
Steve Gravrock <sgravrock@pivotal.io>
2020-10-16 11:52:43 -0700

    Removed broken & unnecessary sshpass install

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,11 +17,5 @@ else
   brew install ansible
 fi
 
-if hash sshpass; then
-  echo "sshpass already installed. Skipping."
-else
-    brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
-fi
-
 [[ -d ~/workspace ]]         || mkdir ~/workspace
 [[ -d ~/workspace/alfalfa ]] || git clone https://github.com/seattle-beach/alfalfa.git ~/workspace/alfalfa
